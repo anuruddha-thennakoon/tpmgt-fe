92b7c046a500533443fe9f6e64e3ddec62c5f40f
Anuruddha Thennakoon <anuruddha.thennakoon@gmail.com>
2017-09-17 01:48:34 +0530

    initialize components angular materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import 'hammerjs';
+
+import {
+  MdInputModule,
+  // MaterialModule,
+  MdDatepickerModule,
+  MdNativeDateModule,
+  MdSliderModule,
+  MdProgressSpinnerModule
+} from '@angular/material';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SideBarComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    MdInputModule,
+    MdDatepickerModule,
+    MdNativeDateModule,
+    MdSliderModule,
+    MdProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
