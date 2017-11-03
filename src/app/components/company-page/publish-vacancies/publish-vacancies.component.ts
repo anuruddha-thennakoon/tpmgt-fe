188e112bf0a07205f2de637ee5aed5f4af235692
CheeBhagyani <cheebhagyani1994@gmail.com>
2017-11-03 21:27:59 +0530

    fix

--- src/app/components/company-page/publish-vacancies/publish-vacancies.component.ts
+++ src/app/components/company-page/publish-vacancies/publish-vacancies.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CompanyService } from '../../../services/callers/company.service';
 import { jobVacancyModel } from '../../../models/jobVacancy.model';
 import { StoreService } from '../../../services/store.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-publish-vacancies',
@@ -14,10 +15,21 @@ export class PublishVacanciesComponent implements OnInit {
 
   constructor(
     private store: StoreService,
-    private company: CompanyService
+    private company: CompanyService,
+    private router: Router
   ) { }
 
   ngOnInit() {
+  }
+
+  goPublishVacancy(){
+    this.company.getALLVacancies()
+      .then(result => {
+        this.store.jobVacancyList = new Array<jobVacancyModel>();
+        this.store.jobVacancyList = JSON.parse(JSON.stringify(result));
+      }).catch(error => {
+        console.log(error);
+      });
   }
 
   setVacancy(jobVacancy) {
@@ -32,6 +44,7 @@ export class PublishVacanciesComponent implements OnInit {
         this.newVacancy.description = "";
         this.newVacancy.duration = "";
         this.newVacancy.paidNonPaid = "";
+        this.goPublishVacancy();
         console.log(result);
       }).catch(error => {
         console.log(error);
